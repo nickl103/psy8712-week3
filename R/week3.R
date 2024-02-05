30ccc87bcacc9091d8a79db33b3016f89961e53b
nickl103 <nickl103@umn.edu>
2024-02-05 11:42:09 -0600

    data import and cleaning complete

--- R/week3.R
+++ R/week3.R
@@ -1,3 +1,9 @@
 #Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
+#Data Import and Cleaning
+raw_df <- read.csv("~/Desktop/psy8712-week3/data/week3.csv")
+raw_df$timeStart <- as.POSIXct(raw_df$timeStart)
+raw_df$timeEnd <- as.POSIXct(raw_df$timeEnd)
+clean_df <- subset(raw_df, raw_df$timeStart > "2017-06-30 23:59:59")
+clean_df <- subset(clean_df, clean_df$q6 == 1)
