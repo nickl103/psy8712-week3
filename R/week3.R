9c7ffd57e535842f950178a5659048a2bf9f8edf
nickl103 <nickl103@umn.edu>
2024-02-05 12:31:25 -0600

    data analysis complete

--- R/week3.R
+++ R/week3.R
@@ -7,3 +7,13 @@ raw_df$timeStart <- as.POSIXct(raw_df$timeStart)
 raw_df$timeEnd <- as.POSIXct(raw_df$timeEnd)
 clean_df <- subset(raw_df, raw_df$timeStart > "2017-06-30 23:59:59")
 clean_df <- subset(clean_df, clean_df$q6 == 1)
+
+#Analysis
+clean_df$timeSpent <- difftime(clean_df$timeStart, clean_df$timeEnd, units="secs")
+hist(as.numeric(clean_df$timeSpent))
+frequency_tables_list <- lapply(clean_df[,5:14],table)
+lapply(frequency_tables_list,barplot)
+print(sum((clean_df$q1>=clean_df$q2) & (clean_df$q2 != clean_df$q3)))
+for(f in frequency_tables_list){
+  print(barplot(f))
+}
